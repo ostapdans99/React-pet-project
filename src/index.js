7d22a05a691b89bfd9913b4bb5185902f168a62f
Ostapenlo Andrei <ostapdans99@gmail.com>
2021-03-02 16:58:31 +0300

    added: Added Redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { App } from "./App"
-import { createStore, compose } from "redux"
+import { createStore, compose, applyMiddleware } from "redux"
 import { Provider } from "react-redux"
+import thunk from "redux-thunk"
 import { mainReducer } from "./reducers/mainReducer"
 
 const store = createStore(
@@ -10,6 +11,7 @@ const store = createStore(
 	compose(
 		window.__REDUX_DEVTOOLS_EXTENSION__ &&
 			window.__REDUX_DEVTOOLS_EXTENSION__(),
+		applyMiddleware(thunk),
 	),
 )
 
